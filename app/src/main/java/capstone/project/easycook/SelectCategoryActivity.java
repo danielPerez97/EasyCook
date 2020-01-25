e5de359d01e272d2cfb716a74687883b4e179c0d
danielPerez97 <doperez1997@gmail.com>
2020-01-25 15:22:26 -0600

    Set up SelectCategoryActivity with a view

---
+++ app/src/main/java/capstone/project/easycook/SelectCategoryActivity.java
@@ -0,0 +1,16 @@
+package capstone.project.easycook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import daniel.perez.easycook.R;
+
+public class SelectCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_category);
+    }
+}
