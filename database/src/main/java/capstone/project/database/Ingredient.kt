950c8f2ce9f5c9a88d834514d6e7d03d0a276cc5
danielPerez97 <doperez1997@yahoo.com>
2020-02-29 15:00:03 -0600

    Fixed the amount type from INTEGER to TEXT in the SQLite database

--- database/src/main/java/capstone/project/database/Ingredient.kt
+++ database/src/main/java/capstone/project/database/Ingredient.kt
@@ -1,3 +1,3 @@
 package capstone.project.database
 
-internal data class Ingredient @JvmOverloads constructor(val id: Long? = null, val name: String, val amount: Int?)
+internal data class Ingredient @JvmOverloads constructor(val id: Long? = null, val name: String, val amount: String?)
