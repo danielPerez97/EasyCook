44cd082a584e695f495b18a0bfd863a06b25a8de
danielPerez97 <doperez1997@yahoo.com>
2020-03-18 21:44:47 -0500

    Set up steps in ViewModel and recyclerview

--- app/src/main/java/capstone/project/easycook/viewmodel/RecipeViewModel.java
+++ app/src/main/java/capstone/project/easycook/viewmodel/RecipeViewModel.java
@@ -4,10 +4,15 @@ import androidx.lifecycle.ViewModel;
 
 import com.squareup.sqldelight.runtime.rx.RxQuery;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import capstone.project.core.ViewRecipe;
 import capstone.project.database.recipe.Database;
+import capstone.project.database.recipe.Step;
+import capstone.project.easycook.model.ViewStep;
 import io.reactivex.Observable;
 import io.reactivex.schedulers.Schedulers;
 
@@ -26,5 +31,18 @@ public class RecipeViewModel extends ViewModel
 
         return RxQuery.mapToOneNonNull(RxQuery.toObservable(database.getRecipeQueries().selectById(id), Schedulers.io()))
                 .map(recipe -> new ViewRecipe(recipe.get_id(), recipe.getName()));
+    }
+
+    public Observable<List<ViewStep>> getSteps(long recipeId)
+    {
+        return RxQuery.mapToList( RxQuery.toObservable( database.getStepsQueries().getStepsFromRecipeId(recipeId) ) )
+                .map((List<Step> steps) -> {
+                    ArrayList<ViewStep> viewSteps = new ArrayList<>();
+                    for(Step step: steps)
+                    {
+                        viewSteps.add(new ViewStep(step.getStep_number(), step.getDescription()));
+                    }
+                    return viewSteps;
+                });
     }
 }
