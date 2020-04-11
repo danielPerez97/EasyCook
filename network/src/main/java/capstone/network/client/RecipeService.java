42f4ece2dfe26d61c82a736cf4481e46c357bfe1
Alejandro Urquieta Salazar <ales.urquieta@gmail.com>
2020-04-11 14:43:01 -0500

    Added interface for endpoints

--- network/src/main/java/capstone/network/client/RecipeService.java
+++ network/src/main/java/capstone/network/client/RecipeService.java
@@ -4,11 +4,28 @@ import java.util.List;
 
 import io.reactivex.Completable;
 import io.reactivex.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface RecipeService
 {
     @GET("/")
     List<Recipe> getAllRecipes();
+
+    @GET("/{id}")
+    Recipe getRecipeId(@Path("id") long id);
+
+    @POST("/")
+    Recipe postRecipe(@Body Recipe recipe);
+
+    @DELETE("/{id}")
+    int deleteRecipe(@Path("id") long id);
+
+    @PUT("/")
+    Recipe updateRecipe(@Body Recipe recipe);
+
 }
