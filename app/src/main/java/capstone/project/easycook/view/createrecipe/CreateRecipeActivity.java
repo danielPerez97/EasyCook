5f7cf2de3ea718c2382b6decffe96d2ba913c131
danielPerez97 <doperez1997@gmail.com>
2020-04-26 14:09:54 -0500

    Added support for choosing a category

--- app/src/main/java/capstone/project/easycook/view/createrecipe/CreateRecipeActivity.java
+++ app/src/main/java/capstone/project/easycook/view/createrecipe/CreateRecipeActivity.java
@@ -2,17 +2,22 @@ package capstone.project.easycook.view.createrecipe;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.jakewharton.rxbinding3.widget.RxAdapterView;
+
 import java.util.List;
 
 import javax.inject.Inject;
 
 import capstone.project.database.Category;
+import capstone.project.easycook.R;
 import capstone.project.easycook.Utils;
 import capstone.project.easycook.databinding.ActivityCreateRecipeBinding;
 import capstone.project.easycook.di.viewmodel.ViewModelFactory;
@@ -34,6 +39,7 @@ public class CreateRecipeActivity extends AppCompatActivity
     AddIngredientAdapter ingredientAdapter = new AddIngredientAdapter();
     AddStepAdapter stepsAdapter = new AddStepAdapter();
     CompositeDisposable disposables = new CompositeDisposable();
+    Category category = Category.BREAKFAST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -44,6 +50,18 @@ public class CreateRecipeActivity extends AppCompatActivity
         setSupportActionBar(binding.toolbar);
         setContentView(binding.getRoot());
         viewModel = new ViewModelProvider(this, factory).get(CreateRecipeViewModel.class);
+
+        // Set up the Spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.selections_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinner.setAdapter(adapter);
+        disposables.add(RxAdapterView.itemSelections(binding.spinner)
+                .subscribe((Integer position) -> {
+                    CharSequence item = (CharSequence) binding.spinner.getItemAtPosition(position);
+                    Log.i("Item", item.toString());
+                    category = Category.valueOf(item.toString().toUpperCase());
+                }));
+
 
         // Set up the ingredients RecyclerView adapter
         binding.ingredientsList.setLayoutManager(new LinearLayoutManager(getBaseContext()));
@@ -74,15 +92,11 @@ public class CreateRecipeActivity extends AppCompatActivity
         disposables.add(viewModel.save(
                 binding.editName.getText().toString(),
                 binding.descTv.getText().toString(),
-                category(),
+                category,
                 ingredients,
                 steps)
         .observeOn( AndroidSchedulers.mainThread() )
         .subscribe(recipe -> finish()));
     }
 
-    private Category category()
-    {
-        return Category.DINNER;
-    }
 }
